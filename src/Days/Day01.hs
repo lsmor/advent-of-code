00edf70b02791bd8549f9a9cb62faf9005cc41fa
Luis <luismorillo@outlook.com>
2020-12-09 18:25:22 +0100

    day 1 completed

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,39 +1,47 @@
 module Days.Day01 (runDay) where
 
-{- ORMOLU_DISABLE -}
+import Data.Attoparsec.Text
 import Data.List
 import Data.Map.Strict (Map)
+import Control.Applicative ((<|>))
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
+import Data.Void (Void)
 import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import qualified Util.Util as U
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Integer]
 
-type OutputA = Void
+type OutputA = Integer
 
-type OutputB = Void
+type OutputB = Integer
+
+aux :: Integer -> Map.Map Integer Integer -> [Integer] -> Maybe (Integer, Integer)
+aux _ _ [] = Nothing
+aux l m (x:xs) = sequenceA (x, Map.lookup x m) <|> aux l newm xs
+  where newm = Map.insert (l - x) x m
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA xs = case aux 2020 Map.empty xs of
+  Nothing -> 0
+  Just (a,b) -> a * b
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB [] = 0
+partB (x:xs) = case aux (2020 - x) Map.empty xs of
+  Nothing -> partB xs
+  Just (a,b) -> x * a * b

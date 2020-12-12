33a5e3adab2bdea5a87ee6101ada55eb89b5b008
Luis <luismorillo@outlook.com>
2020-12-12 12:31:06 +0100

    day 3 completed

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,39 +1,50 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
+import Data.List.PointedList.Circular
+    ( fromList, singleton, moveN, PointedList(_focus) )
+import Control.Applicative ( Alternative((<|>)) )
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
-inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+--inputParser :: Parser Input
+inputParser :: Parser [PointedList Square]
+inputParser = lineToPointed `sepBy` endOfLine
+  where 
+    charToSquare :: Parser Square 
+    charToSquare = (char '.' >> return Open) <|> (char '#' >> return Tree)
+    lineToPointed :: Parser (PointedList Square)
+    lineToPointed = do
+        xs <- many' charToSquare
+        case fromList xs of
+            Nothing -> return $ singleton Open
+            Just p  -> return p
 
 ------------ TYPES ------------
-type Input = Void
+data Square = Open | Tree  deriving (Show, Eq, Enum)
 
-type OutputA = Void
+type Input = [PointedList Square]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
+
+moveInput :: Int -> Int -> Input -> Input
+moveInput a b =  drop b . fmap (moveN a)
+
+countTrees :: Int -> Int -> Input -> OutputA
+countTrees _ _ [] = 0
+countTrees a b (x:xs) = fromEnum (_focus x) + countTrees a b (moveInput a (b-1) xs)
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = countTrees 3 1
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB i = product $ ($ i) <$> [countTrees 1 1, countTrees 3 1, countTrees 5 1, countTrees 7 1, countTrees 1 2]

d9119053be100fc17bea8b1742cc36c3f7c33db5
Luis <luismorillo@outlook.com>
2020-12-13 13:26:34 +0100

    completed day 4

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -46,68 +46,124 @@ validate f t = if f t then Just t else Nothing
 
 passPortFromMap :: Map.Map Text Text -> Maybe PassPort 
 passPortFromMap m = do
-    
-    pid <- "pid" `Map.lookup` m >>= validate ((== 9) . T.length)
-    readMaybe @Int (unpack pid)
-    
-    eyr <- "eyr" `Map.lookup` m 
-                >>= validate ((== 4) . T.length)
-                >>= readMaybe . unpack 
-                >>= validate (2020 <=)
-                >>= validate (<= 2030)
 
-    byr <- "byr" `Map.lookup` m
-                >>= validate ((== 4) . T.length)
-                >>= readMaybe . unpack 
-                >>= validate (1920 <=)
-                >>= validate (<= 2002)
-
-    iyr <- "iyr" `Map.lookup` m
-                >>= validate ((== 4) . T.length)
-                >>= readMaybe . unpack 
-                >>= validate (2010 <=)
-                >>= validate (<= 2020)
-
-    ecl <- "ecl" `Map.lookup` m 
-                >>= validate (`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
-
-    hcl <- "hcl" `Map.lookup` m
-                >>= \t -> case T.uncons t of 
-                            Just ('#', ts) -> validate (T.all isHexDigit) ts
-                            _              -> Nothing
-
-    let hgtparse :: Parser (Maybe Text)
-        hgtparse = do
-            n <- decimal
-            u <- takeText
-            case u of
-                "cm" -> if 150 <= n && n <= 193 then return (Just $ T.pack $ show n) else return Nothing
-                "in" -> if 59 <= n && n <= 76 then return (Just $ T.pack $ show n) else return Nothing
-
-    mhgt <- "hgt" `Map.lookup` m >>= maybeResult . parse hgtparse
-    hgt <- mhgt
-    let cid = "cid" `Map.lookup` m
+    pid <- Pid <$> "pid" `Map.lookup` m
+    eyr <- Eyr <$> ("eyr" `Map.lookup` m >>= readMaybe . unpack)
+    byr <- Byr <$> ("byr" `Map.lookup` m >>= readMaybe . unpack)
+    iyr <- Iyr <$> ("iyr" `Map.lookup` m >>= readMaybe . unpack)
+    ecl <- Ecl <$> "ecl" `Map.lookup` m
+    hcl <- Hcl <$> "hcl" `Map.lookup` m
+    hgt <- Hgt <$> "hgt" `Map.lookup` m
+    let cid = Cid $ "cid" `Map.lookup` m
 
     return PassPort {..}
 
-passPortParser :: Parser (Maybe PassPort)
-passPortParser = do
+example :: Text
+example = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f"
+
+passPortFromMapValidate :: Map.Map Text Text -> Maybe PassPort 
+passPortFromMapValidate m = do
+    pid <- "pid" `Map.lookup` m >>= validateField
+    eyr <- "eyr" `Map.lookup` m >>= validateField
+    byr <- "byr" `Map.lookup` m >>= validateField
+    iyr <- "iyr" `Map.lookup` m >>= validateField
+    ecl <- "ecl" `Map.lookup` m >>= validateField
+    hcl <- "hcl" `Map.lookup` m >>= validateField
+    hgt <- "hgt" `Map.lookup` m >>= validateField
+    let cid = Cid $ "cid" `Map.lookup` m
+
+    return PassPort {..}
+
+passPortParser :: (Map.Map Text Text -> Maybe PassPort) -> Parser (Maybe PassPort)
+passPortParser f = do
     kv <- fieldsParser
     let m = Map.fromList kv
-    return $ passPortFromMap m
+    return $ f m
 
 inputParser :: Parser Input
-inputParser = catMaybes <$> passPortParser `sepBy` endOfLine
+inputParser = catMaybes <$> passPortParser passPortFromMapValidate `sepBy` endOfLine
 
 ------------ TYPES ------------
-data PassPort = PassPort { pid :: Text
-                         , cid :: Maybe Text
-                         , eyr :: Int
-                         , byr :: Int
-                         , iyr :: Int
-                         , ecl :: Text
-                         , hcl :: Text
-                         , hgt :: Text
+class IsValidPassPortField a where
+    validateField :: Text -> Maybe a
+
+newtype Pid = Pid Text deriving (Show, Eq)
+instance IsValidPassPortField Pid where
+    validateField t = do 
+        validate ((== 9) . T.length) t
+        validate (T.all isNumber) t
+        return $ Pid t
+
+newtype Cid = Cid (Maybe Text) deriving (Show, Eq)
+
+newtype Eyr = Eyr Int deriving (Show, Eq)
+instance IsValidPassPortField Eyr where
+    validateField t = do
+        validate ((== 4) . T.length) t
+        i <- readMaybe @Int (unpack t)
+        validate (<= 2030) i
+        validate (2020 <=) i
+        return $ Eyr i
+
+newtype Byr = Byr Int deriving (Show, Eq)
+instance IsValidPassPortField Byr where
+    validateField t = do
+        validate ((== 4) . T.length) t
+        i <- readMaybe @Int (unpack t)
+        validate (<= 2002) i
+        validate (1920 <=) i
+        return $ Byr i
+
+newtype Iyr = Iyr Int deriving (Show, Eq)
+instance IsValidPassPortField Iyr where
+    validateField t = do
+        validate ((== 4) . T.length) t
+        i <- readMaybe @Int (unpack t)
+        validate (<= 2020) i
+        validate (2010 <=) i
+        return $ Iyr i
+
+newtype Ecl = Ecl Text deriving (Show, Eq)
+instance IsValidPassPortField Ecl where
+    validateField t = do 
+        validate (`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]) t
+        return $ Ecl t
+
+newtype Hcl = Hcl Text deriving (Show, Eq)
+instance IsValidPassPortField Hcl where
+    validateField t = do 
+        (c, t1) <- T.uncons t
+        validate (== '#') c
+        validate ((== 6) . T.length) t1
+        validate (T.all isHexDigit) t1
+        return $ Hcl t
+
+newtype Hgt = Hgt Text deriving (Show, Eq)
+instance IsValidPassPortField Hgt where
+    validateField t = do 
+        (t1, c1) <- T.unsnoc t
+        (t2, c0) <- T.unsnoc t1
+        let u = c0 `T.cons` c1 `T.cons` ""
+        i <- readMaybe @Int (unpack t2)
+        case u of 
+            "cm" -> do
+                validate (<= 193) i
+                validate (150 <=) i
+                return $ Hgt t
+            "in" -> do
+                validate (<= 76) i
+                validate (59 <=) i
+                return $ Hgt t
+            _    -> Nothing
+
+data PassPort = PassPort { pid :: Pid
+                         , cid :: Cid
+                         , eyr :: Eyr
+                         , byr :: Byr
+                         , iyr :: Iyr
+                         , ecl :: Ecl
+                         , hcl :: Hcl
+                         , hgt :: Hgt
                          } deriving (Show, Eq)
 
 type Input = [PassPort]
@@ -118,7 +174,7 @@ type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = length
+partA = length 
 
 ------------ PART B ------------
 partB :: Input -> OutputB
